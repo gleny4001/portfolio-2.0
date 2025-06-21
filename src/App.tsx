cb4492f7539baef24f1ae852f95a7fd3f3bbaa23
glen <gleny4001@gmail.com>
2025-06-20 20:20:47 -0700

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import Title from "./components/Title"
-import About from "./components/About";
-import Header from "./components/Header";
-import Work from "./components/Work";
-import Skills from "./components/Skills";
-import Nav from "./components/Nav";
-import Education from "./components/Education";
-import MainProject from "./components/Projects/MainProject";
-import AIChat from "./components/AIChat";
+import Title from './components/Title';
+import About from './components/About';
+import Header from './components/Header';
+import Work from './components/Work';
+import Skills from './components/Skills';
+import Nav from './components/Nav';
+import Education from './components/Education';
+import MainProject from './components/Projects/MainProject';
+import AIChat from './components/AIChat';
 
 function App() {
-
-
-  return (
-    <div className="w-full min-h-screen bg-background">
-      <Header/>
-      <Title/>
-      <About/>
-      <Work/>
-      <Skills/>
-      <MainProject/>
-      <Education/>
-      <Nav/>
-      <AIChat/>
-    </div>
-  )
+    return (
+        <div className="min-h-screen w-full bg-background">
+            <Header />
+            <Title />
+            <About />
+            <Work />
+            <Skills />
+            <MainProject />
+            <Education />
+            <Nav />
+            <AIChat />
+        </div>
+    );
 }
 
-export default App
+export default App;
